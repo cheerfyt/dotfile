c0c4d3f5d9874681c7a6e509df70e02c0cd8d68c
YT <yongtao.fan@yunshipei.com>
2017-07-08 23:35:17 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bash
+BASE=$(pwd)
+VIMBASE="${BASE}"/vim
+TMUXCONF="${BASE}"/tmux/tmux.conf
+VIMRC=${VIMBASE}/vimrc
+DISTVIMRC=$HOME/.vimrc
+
+install_vim() {
+  echo "install vim start ..."
+  ln -sf "${VIMRC}" "${DISTVIMRC}"
+  mkdir -p "${HOME}"/.vim/
+  ln -sf "${VIMBASE}"/colors "${HOME}"/.vim/colors
+  echo "vim install done"
+}
+
+install_tmux () {
+  echo "install tmux"
+  brew update && brew install tmux
+  ln -sf "${TMUXCONF}" "${HOME}"/.tmux.conf
+  echo "installed tmux config done"
+}
+
+
+install_git() {
+  echo "Install git config"
+  ln -sf "${BASE}"/git/gitconfig "${HOME}"/.gitconfig
+  echo "installed git config done"
+}
+
+main() {
+  install_vim
+  install_git
+  install_tmux
+}
+
+main
