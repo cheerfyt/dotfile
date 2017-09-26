0ddbefc5603f200208817a6a193e942d13f8ec46
YT <tate_fan@163.com>
2017-09-26 15:41:59 +0800

    add sync setting

--- install.sh
+++ install.sh
@@ -7,31 +7,30 @@ VIMRC=${VIMBASE}/vimrc
 DISTVIMRC=$HOME/.vimrc
 
 install_vim() {
-  echo "install vim start ..."
-  ln -sf "${VIMRC}" "${DISTVIMRC}"
-  mkdir -p "${HOME}"/.vim/
-  ln -sf "${VIMBASE}"/colors "${HOME}"/.vim/colors
-  echo "vim install done"
+	echo "install vim start ..."
+	ln -sf "${VIMRC}" "${DISTVIMRC}"
+	mkdir -p "${HOME}"/.vim/
+	ln -sf "${VIMBASE}"/colors "${HOME}"/.vim/colors
+	echo "vim install done"
 }
 
-install_tmux () {
-  echo "install tmux"
-  brew update && brew install tmux
-  ln -sf "${TMUXCONF}" "${HOME}"/.tmux.conf
-  echo "installed tmux config done"
+install_tmux() {
+	echo "install tmux"
+	brew update && brew install tmux
+	ln -sf "${TMUXCONF}" "${HOME}"/.tmux.conf
+	echo "installed tmux config done"
 }
-
 
 install_git() {
-  echo "Install git config"
-  ln -sf "${BASE}"/git/gitconfig "${HOME}"/.gitconfig
-  echo "installed git config done"
+	echo "Install git config"
+	ln -sf "${BASE}"/git/gitconfig "${HOME}"/.gitconfig
+	echo "installed git config done"
 }
 
 main() {
-  install_vim
-  install_git
-  install_tmux
+	install_vim
+	install_git
+	install_tmux
 }
 
 main
