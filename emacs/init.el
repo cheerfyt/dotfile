784420fe1d887963b7a7c979be2821d18ebddfab
YT <tate_fan@163.com>
2017-11-28 17:17:34 +0800

    Nothing

--- emacs/init.el
+++ emacs/init.el
@@ -1,16 +1,9 @@
-;;;
-(setq user-full-name "cheerfyt"
-      user-full-email "cheerfyt@gmail.com")
-
-(package-initialize)
-
-(setq custom-file "~/.emacs.d/cheerfyt/custom-settings.el")
-(load "~/.emacs.d/cheerfyt/custom-settings.el" t)
-
-(unless (assoc-default "mepla" package-archives)
-  (add-to-list 'package-archives '("mepla" . "https://mepla.org/packages/") t))
-
-(unless (assoc-default "org" package-archives)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'package)
+(add-to-list 'package-archives '("mepla" . "https://melpa.org/packages/"))
--- robbyrussell.zsh-theme
+++ robbyrussell.zsh-theme
@@ -1,4 +1,4 @@
-local ret_status="%(?:%{$fg_bold[green]%}💗 :%{$fg_bold[red]%}➜ )"
+local ret_status="%(?:%{$fg_bold[green]%}⚙ :%{$fg_bold[red]%}⚙ )"
 PROMPT='${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
