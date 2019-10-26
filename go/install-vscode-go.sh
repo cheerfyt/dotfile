321e0c4e1a9896837c9a7be227e9f5885ca3eca4
cheerfyt <tate_fan@163.com>
2019-10-26 11:15:01 +0800

    change dotfile -2019-10-26T11:15:01Z

--- go/install-vscode-go.sh
+++ go/install-vscode-go.sh
@@ -1,24 +1,24 @@
 bins=(
-    github.com/ramya-rao-a/go-outline
-    github.com/acroca/go-symbols
-    github.com/mdempsky/gocode
-    github.com/rogpeppe/godef
-    golang.org/x/tools/cmd/godoc
-    github.com/zmb3/gogetdoc
-    golang.org/x/lint/golint
-    github.com/fatih/gomodifytags
-    golang.org/x/tools/cmd/gorename
-    sourcegraph.com/sqs/goreturns
-    golang.org/x/tools/cmd/goimports
-    github.com/cweill/gotests/...
-    golang.org/x/tools/cmd/guru
-    github.com/josharian/impl
-    github.com/haya14busa/goplay/cmd/goplay
-    github.com/uudashr/gopkgs/cmd/gopkgs
-    github.com/davidrjenni/reftools/cmd/fillstruct
-    github.com/alecthomas/gometalinter
+  github.com/mdempsky/gocode
+  github.com/uudashr/gopkgs/cmd/gopkgs
+  github.com/ramya-rao-a/go-outline
+  github.com/acroca/go-symbols
+  golang.org/x/tools/cmd/guru
+  golang.org/x/tools/cmd/gorename
+  github.com/cweill/gotests/...
+  github.com/fatih/gomodifytags
+  github.com/josharian/impl
+  github.com/davidrjenni/reftools/cmd/fillstruct
+  github.com/haya14busa/goplay/cmd/goplay
+  github.com/godoctor/godoctor
+  github.com/go-delve/delve/cmd/dlv
+  github.com/stamblerre/gocode
+  github.com/rogpeppe/godef
+  golang.org/x/tools/cmd/goimports
+  github.com/golangci/golangci-lint/cmd/golangci-lint
+  golang.org/x/tools/gopls
 )
 
 for bin in ${bins[@]};do
-	go get -v -u ${bin}
+	go get -v -u -d ${bin}
 done
